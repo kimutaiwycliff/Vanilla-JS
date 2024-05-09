a659b3464830cd193c21790e58601ce3461c242a
Wycliff Kimutai <kcliff@tutanota.com>
2024-05-09 12:03:00 +0300

    feat(js:core):Added routing and bookmark component

--- rmtDev/src/components/JobList.js
+++ rmtDev/src/components/JobList.js
@@ -3,6 +3,7 @@ import {
     RESULTS_PER_PAGE,
     state,
     jobListSearchEl,
+    jobListBookmarksEl,
     jobDetailsContentEl,
     getData
 } from '../common.js';
@@ -10,14 +11,25 @@ import renderSpinner from './Spinner.js';
 import renderJobDetails from './JobDetails.js';
 import renderError from './Error.js';
 
-const renderJobList = () => {
+const renderJobList = (whichJobList = 'search') => {
+    // determine correct selector for job list (search results list or bookmarks list)
+    const jobListEl = whichJobList === 'search' ? jobListSearchEl : jobListBookmarksEl;
+
     // remove previous job items
-    jobListSearchEl.innerHTML = '';
+    jobListEl.innerHTML = '';
+
+    // determine the job items that should be rendered
+    let jobItems;
+    if (whichJobList === 'search') {
+        jobItems = state.searchJobItems.slice(state.currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE, state.currentPage * RESULTS_PER_PAGE);
+    } else if (whichJobList === 'bookmarks') {
+        jobItems = state.bookmarkJobItems;
+    }
 
     // display job items
-    state.searchJobItems.slice(state.currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE, state.currentPage * RESULTS_PER_PAGE).forEach(jobItem => {
+    jobItems.forEach(jobItem => {
         const newJobItemHTML = `
-            <li class="job-item">
+            <li class="job-item ${state.activeJobItem.id === jobItem.id ? 'job-item--active' : ''}">
                 <a class="job-item__link" href="${jobItem.id}">
                     <div class="job-item__badge">${jobItem.badgeLetters}</div>
                     <div class="job-item__middle">
@@ -36,7 +48,7 @@ const renderJobList = () => {
                 </a>
             </li>
         `;
-        jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
+        jobListEl.insertAdjacentHTML('beforeend', newJobItemHTML);
     });
 };
 
@@ -47,8 +59,8 @@ const clickHandler = async event => {
     // get clicked job item element
     const jobItemEl = event.target.closest('.job-item');
 
-    // remove the active class from previously active job item
-    document.querySelector('.job-item--active')?.classList.remove('job-item--active');
+    // remove the active class from previously active job items
+    document.querySelectorAll('.job-item--active').forEach(jobItemWithActiveClass => jobItemWithActiveClass.classList.remove('job-item--active'));
 
     // add active class
     jobItemEl.classList.add('job-item--active');
@@ -61,6 +73,12 @@ const clickHandler = async event => {
 
     // get the id
     const id = jobItemEl.children[0].getAttribute('href');
+
+    // update state
+    state.activeJobItem = state.searchJobItems.find(jobItem => jobItem.id === +id);
+
+    // add id to url
+    history.pushState(null, '', `/#${id}`);
 
     try {
         // fetch job item data
@@ -81,5 +99,6 @@ const clickHandler = async event => {
 };
 
 jobListSearchEl.addEventListener('click', clickHandler);
+jobListBookmarksEl.addEventListener('click', clickHandler);
 
 export default renderJobList;
